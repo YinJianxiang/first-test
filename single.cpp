b6c8b0b3659d18d7c967f6c2785e0b4328235b7d
YinJianxiang <YinjIanxiang123@gmail.com>
2017-10-31 18:47:31 +0800

    single.cpp

---
+++ single.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+class Singleton
+{
+public:
+    static Singleton *GetInstance()
+    {
+        if (instance_ == NULL)
+        {
+            instance_ = new Singleton;
+        }
+        return instance_;
+    }
+
+    ~Singleton()
+    {
+        cout << "~Singleton ..." << endl;
+    }
+private:
+    Singleton(const Singleton &other);
+    Singleton &operator=(const Singleton &other);
+    Singleton()
+    {
+        cout << "Singleton ..." << endl;
+    }
+    static Singleton *instance_;
+};
+
+Singleton *Singleton::instance_;
+
+int main(void)
+{
+    //Singleton s1;
+    //Singleton s2;
+
+    Singleton *s1 = Singleton::GetInstance();
+    Singleton *s2 = Singleton::GetInstance();
+
+    //Singleton s3(*s1);        // 调用拷贝构造函数
+
+    return 0;
+}
+
