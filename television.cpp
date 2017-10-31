786fe53878ce7c7648810140397992545e6c89f4
YinJianxiang <YinjIanxiang123@gmail.com>
2017-10-31 18:51:22 +0800

    television.cpp

---
+++ television.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+    > File Name: television.cpp
+    > Author: YinJianxiang
+    > Mail: YinJianxiang123@gmail.com
+    > Created Time: 2017年10月21日 星期六 22时06分03秒
+ ************************************************************************/
+
+#include "television.h"
+
+Television::Television(int volume,int chanel) : volume_(volume) , chanel_(chanel) {
+
+}
+
